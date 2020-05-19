4a487da671cdbf9b1d635d5fbda49f253d830707
dkbrz <glaz.dikobraz@gmail.com>
2020-05-19 09:54:24 +0300

    generate df in separate file

--- visualization/dash_plotly/dash_project/dash_app.py
+++ visualization/dash_plotly/dash_project/dash_app.py
@@ -1,40 +1,16 @@
-import pandas as pd
-import numpy as np
-
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 import plotly.graph_objs as go
 
+from generate_df import generate_df
 
-letters = [let for let in 'abcdefghijklmnopqrstuvwxyz']
-
-def generate_name():
-    n1 = np.random.randint(3, 5)
-    n2 = np.random.randint(3, 10)
-    w1 = ''.join(np.random.choice(letters, n1)).capitalize()
-    w2 = ''.join(np.random.choice(letters, n2)).capitalize()
-    return f"{w1} {w2}"
-
-K = 250
-df = pd.DataFrame({
-    'name': [generate_name() for _ in range(K)],
-    'test_0': [np.round(100 * i / K + np.random.randint(-5, 5)) for i in range(K)],
-    'test_1': [np.round(100 * i / K + np.random.randint(-15, 15)) + 10 for i in range(K)],
-    'group': [np.random.randint(6) for _ in range(K)]
-})
-df.loc[df['test_0'] < 0, 'test_0'] = 0
-df.loc[df['test_0'] > 100, 'test_0'] = 100
-df.loc[df['test_1'] < 0, 'test_1'] = 0
-df.loc[df['test_1'] > 100, 'test_1'] = 100
-
-df['passed'] = df['test_1'].apply(lambda x: x > 40)
+df = generate_df()
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
 
 
 app.layout = html.Div([
@@ -59,7 +35,6 @@ def update_graph(dropdown_var):
         df2 = df.copy()
     df2 = df2.groupby("passed").agg({"name": "count"}).reset_index()
     df2['passed'] = df2['passed'].apply(lambda x: 'passed' if x else "failed")
-    sub_df = df[df["group"] == dropdown_var]
     fig = go.Figure(data=[
         go.Pie(
             name='Passed', values=df2['name'], labels=df2['passed'], textinfo='label+value+percent', 
@@ -67,7 +42,7 @@ def update_graph(dropdown_var):
         )
     ])
     fig.update_layout(
-        title = "Passed / failed",
+        title="Passed / failed",
         height=500,
         width=500
     )
---
+++ visualization/dash_plotly/dash_project/generate_df.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import numpy as np
+
+letters = [let for let in 'abcdefghijklmnopqrstuvwxyz']
+
+
+def generate_name():
+    n1 = np.random.randint(3, 5)
+    n2 = np.random.randint(3, 10)
+    w1 = ''.join(np.random.choice(letters, n1)).capitalize()
+    w2 = ''.join(np.random.choice(letters, n2)).capitalize()
+    return f"{w1} {w2}"
+
+
+def generate_df():
+    K = 250
+    df = pd.DataFrame({
+        'name': [generate_name() for _ in range(K)],
+        'test_0': [np.round(100 * i / K + np.random.randint(-5, 5)) for i in range(K)],
+        'test_1': [np.round(100 * i / K + np.random.randint(-15, 15)) + 10 for i in range(K)],
+        'group': [np.random.randint(6) for _ in range(K)]
+    })
+    df.loc[df['test_0'] < 0, 'test_0'] = 0
+    df.loc[df['test_0'] > 100, 'test_0'] = 100
+    df.loc[df['test_1'] < 0, 'test_1'] = 0
+    df.loc[df['test_1'] > 100, 'test_1'] = 100
+
+    df['passed'] = df['test_1'].apply(lambda x: x > 40)
+    return df
