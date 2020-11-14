e98c23b3625ed76878c489e208f283427b6d2305
dkbrz <glaz.dikobraz@gmail.com>
2020-11-14 16:12:25 +0300

    app

--- flask_applications/imdb_site/app.py
+++ flask_applications/imdb_site/app.py
@@ -1,9 +1,40 @@
-from flask import Flask
+from flask import Flask, render_template
+from models import Film, Person, Crew, db, Rating
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///imdb_small_indexed.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.app = app
+db.init_app(app)
+
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+
+@app.route("/film/<film_id>")
+def film_page(film_id):
+    film = Film.query.get(film_id)
+    return render_template("film.html", film=film)
+
+
+@app.route("/person/<person_id>")
+def person_page(person_id):
+    person = Person.query.get(person_id)
+    return render_template("person.html", person=person)
+
+
+@app.route("/rating")
+def rating():
+    # rating = Film.query.limit(250).all() # .order_by(Film.rating.value)
+    rating = db.session.query(Film)\
+        .join(Rating)\
+        .filter(Rating.votes > 100000)\
+        .order_by(-Rating.value)\
+        .limit(250).all()
+    return render_template("rating.html", rating=rating)
+
 
 if __name__ == '__main__':
     app.run()
---
+++ flask_applications/imdb_site/models.py
@@ -0,0 +1,62 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import ForeignKey, PrimaryKeyConstraint
+from sqlalchemy.orm import relationship
+
+db = SQLAlchemy()
+
+
+class Film(db.Model):
+    __tablename__ = "titles"
+
+    # имя колонки = специальный тип (тип данных, первичный ключ)
+    film_id = db.Column('title_id', db.Integer, primary_key=True)
+
+    # соединяем
+    film_type_id = db.Column('type', db.Integer, ForeignKey('film_types.id'))
+    film_type = db.relationship('Type')
+
+    rating = db.relationship('Rating', uselist=False, primaryjoin="Film.film_id==Rating.film_id")
+
+    title = db.Column('title', db.Text)
+    premiered = db.Column('premiered', db.Integer)
+    ended = db.Column('ended', db.Integer)
+    runtime_min = db.Column('runtime_min', db.Integer)
+
+    crew = db.relationship("Person", secondary='crew')
+
+
+class Type(db.Model):
+
+    __tablename__ = "film_types"
+
+    type_id = db.Column('id', db.Integer, primary_key=True)
+    name = db.Column('film_type', db.Text)
+
+
+class Person(db.Model):
+    __tablename__ = "people"
+
+    person_id = db.Column('person_id', db.Integer, primary_key=True)
+    name = db.Column('name', db.Text)
+    born = db.Column('born', db.Integer)
+    died = db.Column('died', db.Integer)
+
+    films = relationship("Film", secondary='crew')
+
+
+class Crew(db.Model):
+    __tablename__ = 'crew'
+    __table_args__ = (PrimaryKeyConstraint('title_id', 'person_id'),)
+
+    film_id = db.Column('title_id', db.Integer, ForeignKey('titles.title_id'))
+    person_id = db.Column('person_id', db.Integer, ForeignKey('people.person_id'))
+
+
+class Rating(db.Model):
+
+    __tablename__ = "rating"
+
+    film_id = db.Column('title_id', db.Integer, ForeignKey('titles.title_id'), primary_key=True)
+    value = db.Column('rating', db.Float)
+    votes = db.Column('votes', db.Integer)
